0e80b8acf449e998f45ae8fc87365ecaaeb99379
Ramiro Mendez <ramiromendez@XIFRA-SIS006s-MacBook-Pro.local>
2021-07-10 12:22:39 -0500

    File filter

--- src/middlewares/img.middleware.js
+++ src/middlewares/img.middleware.js
@@ -12,4 +12,13 @@ const storage = multer.diskStorage({
     }
 });
 
-export const imageHandler = multer({ storage }).single('image');
+export const imageHandler = multer({
+    storage,
+    fileFilter: (req, file, cb) => {
+        const types = /jpeg|jpg|png|gif/;
+        const mimetype = types.test(file.mimetype);
+        const extension = types.test(path.extname(file.originalname));
+        return mimetype && extension ? cb(null, true) : cb('File must be image type (jpeg | jpg | png | gif)')
+    }
+
+}).single('image');
